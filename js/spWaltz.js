180e993c6dc750a8697ddd4c519134e3aeb7fef5
taejaehan <hantaejae@gmail.com>
2017-04-09 20:13:56 +0900

    random res

--- js/spWaltz.js
+++ js/spWaltz.js
@@ -4,6 +4,14 @@
 // }
 
 
+var _checkMobile = (function () {
+   var check = false;
+   (function (a) {
+       if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) check = true
+   })(navigator.userAgent || navigator.vendor || window.opera);
+   return check;
+})();
+
 var spwDraw = spwDraw || {
   context : [],
   drawImageProp : function(ctx, img, x, y, w, h, offsetX, offsetY) {
@@ -294,7 +302,7 @@ var springWaltz = springWaltz || function(w, h, ctx, back, audio){
       _curMouseEvent = 'mousemove',
       _averageMeting = 0;
 
-      _iconW = _iconH = 50;
+      _iconW = _iconH = 30;
       _fbImg = new Image;
       _fbImg.src = 'resources/imgs/fb.png';
       _twImg = new Image;
@@ -401,8 +409,13 @@ var springWaltz = springWaltz || function(w, h, ctx, back, audio){
   };
 
   function drawLoading(){
+    var loadCircle = _playBtnRadius/3;
+    var loadCenter = [_canvasCen[0] - (_playBtnRadius/3), _canvasCen[1]];
+    var loadColor = "gray";
+    _loadAngle += 0.1;
+
     _context.beginPath();
-    _context.fillStyle =  "red";
+    _context.fillStyle =  loadColor;
     _context.lineWidth = 3;
     spwDraw.drawCell(spwVo.triangles[spwVo.triPlayIndex]);
     _context.stroke();
@@ -410,15 +423,14 @@ var springWaltz = springWaltz || function(w, h, ctx, back, audio){
     _context.closePath();
 
     _context.beginPath();
-    _context.arc(_canvasCen[0] - (_playBtnRadius/2), _canvasCen[1], 20, 0, 2 * Math.PI, false);
+    _context.arc(loadCenter[0], loadCenter[1], loadCircle, 0, 2 * Math.PI, false);
     _context.strokeStyle =  "rgba(255,255,255,1)";
     _context.stroke();
     _context.closePath();
 
     _context.beginPath();
-    _loadAngle += 0.05;
-    _context.arc(_canvasCen[0] - (_playBtnRadius/2) + Math.sin(_loadAngle) * 20, _canvasCen[1] + Math.cos(_loadAngle) * 20, 2.5, 0, 2 * Math.PI, false);
-    _context.fillStyle =  "blue";
+    _context.arc(loadCenter[0] + Math.sin(_loadAngle) * loadCircle, loadCenter[1] + Math.cos(_loadAngle) * loadCircle, 3, 0, 2 * Math.PI, false);
+    _context.fillStyle =  loadColor;
     _context.fill();
     _context.closePath();
   }
@@ -499,7 +511,7 @@ var springWaltz = springWaltz || function(w, h, ctx, back, audio){
         })).triangles();
       }
       
-      if(_averageMeting < 0.85){
+      if(_averageMeting < 0.25){
 
         // spwVo.polygons.forEach(function(p, i){
         //   _context.beginPath();
@@ -615,9 +627,9 @@ var springWaltz = springWaltz || function(w, h, ctx, back, audio){
       var x = d.data[0];
       var y = d.data[1];
       var endingBox = ( x > limitX && x < (_width - limitX) && y > limitY && y < (_height - limitY));
-      if(!endingBox){
-          nextPolygons.push([d.cellIndex, spwUtils.getDistance([_width/2,_height/2], [x,y])]);
-      };
+      // if(!endingBox){
+      //     nextPolygons.push([d.cellIndex, spwUtils.getDistance([_width/2,_height/2], [x,y])]);
+      // };
       return endingBox;
     }));
 
@@ -643,15 +655,16 @@ var springWaltz = springWaltz || function(w, h, ctx, back, audio){
       _context.fill();
       _context.closePath();
 
-      
-      if(nextPolygons[0][0] === i){
-        // _context.fillStyle =  "red";
-        var pos = spwVo.samples[i];
-        _context.drawImage(_fbImg, pos[0] - _iconW/2, pos[1] - _iconH/2, _iconW, _iconH);
-      }else if(nextPolygons[10][0] === i){
-        var pos = spwVo.samples[i];
-        _context.drawImage(_twImg, pos[0] - _iconW/2, pos[1] - _iconH/2, _iconW, _iconH);
-      }
+      _context.drawImage(_fbImg, _width - _iconW * 1.5, 10, _iconW, _iconH);
+      _context.drawImage(_twImg, _width - _iconW * 2.5, 10, _iconW, _iconH);
+      // if(nextPolygons[0][0] === i){
+      //   // _context.fillStyle =  "red";
+      //   var pos = spwVo.samples[i];
+      //   _context.drawImage(_fbImg, pos[0] - _iconW/2, pos[1] - _iconH/2, _iconW, _iconH);
+      // }else if(nextPolygons[10][0] === i){
+      //   var pos = spwVo.samples[i];
+      //   _context.drawImage(_twImg, pos[0] - _iconW/2, pos[1] - _iconH/2, _iconW, _iconH);
+      // }
       
     });
 
@@ -880,10 +893,10 @@ var springWaltz = springWaltz || function(w, h, ctx, back, audio){
 var dreamSpring = dreamSpring || new function(){
 
   var _isSupport = true,
-      _isMobile = true,
+      _isMobile = _checkMobile,
       _mouseholding = true,
-      _audioPath = 'resources/audios/spring_waltz.mp3',
-      _imgPath = 'resources/imgs/sp4.jpg',
+      _imgNum = 4,
+      _imgPath = 'resources/imgs/sp'+Math.floor(Math.random()* _imgNum + 1) +'.jpg',
       _mouseTimeOut,
       _springWaltz,
       _canvas, 
@@ -989,3 +1002,4 @@ var dreamSpring = dreamSpring || new function(){
 
   return _this;
 };
+
